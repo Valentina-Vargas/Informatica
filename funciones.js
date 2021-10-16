e831e0914eb3e77496339578921bf832be98ca6c
Valentina-Vargas <89594485+Valentina-Vargas@users.noreply.github.com>
2021-10-15 21:19:58 -0500

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,98 @@
+document.write("SISTEMAS NUMERICOS <br>");
+
+document.write("<br>Numero 0000");
+
+var numero;
+
+numero = 0* Math.pow(2, 0) + 0* Math.pow(2, 1) + 0* Math.pow(2, 2) + 0* Math.pow(2, 3);   
+
+document.write("<br>El resultado es: "+numero +"<br><br>"); 
+
+document.write("<br>Numero 0001");
+
+numero = 1* Math.pow(2, 0) + 0* Math.pow(2, 1) + 0* Math.pow(2, 2) + 0* Math.pow(2, 3);  
+
+document.write("<br>El resultado es: "+numero +"<br><br>"); 
+
+document.write("<br>Numero 0010");
+
+numero = 0* Math.pow(2, 0) + 1* Math.pow(2, 1) + 0* Math.pow(2, 2) + 0* Math.pow(2, 3);  
+
+document.write("<br>El resultado es: "+numero +"<br><br>"); 
+
+document.write("<br>Numero 0011");
+
+numero = 1* Math.pow(2, 0) + 1* Math.pow(2, 1) + 0* Math.pow(2, 2) + 0* Math.pow(2, 3);  
+
+document.write("<br>El resultado es: "+numero +"<br><br>"); 
+
+document.write("<br>Numero 0100");
+
+numero = 0* Math.pow(2, 0) + 0* Math.pow(2, 1) + 1* Math.pow(2, 2) + 0* Math.pow(2, 3);  
+
+document.write("<br>El resultado es: "+numero +"<br><br>"); 
+
+document.write("<br>Numero 0101");
+
+numero = 1* Math.pow(2, 0) + 0* Math.pow(2, 1) + 1* Math.pow(2, 2) + 0* Math.pow(2, 3);  
+
+document.write("<br>El resultado es: "+numero +"<br><br>");
+
+document.write("<br>Numero 0110");
+
+numero = 0* Math.pow(2, 0) + 1* Math.pow(2, 1) + 1* Math.pow(2, 2) + 0* Math.pow(2, 3);  
+
+document.write("<br>El resultado es: "+numero +"<br><br>");
+
+document.write("<br>Numero 0111");
+
+numero = 1* Math.pow(2, 0) + 1* Math.pow(2, 1) + 1* Math.pow(2, 2) + 0* Math.pow(2, 3);  
+
+document.write("<br>El resultado es: "+numero +"<br><br>");
+
+document.write("<br>Numero 1000");
+
+numero = 0* Math.pow(2, 0) + 0* Math.pow(2, 1) + 0* Math.pow(2, 2) + 1* Math.pow(2, 3);  
+
+document.write("<br>El resultado es: "+numero +"<br><br>");
+
+document.write("<br>Numero 1001");
+
+numero = 1* Math.pow(2, 0) + 0* Math.pow(2, 1) + 0* Math.pow(2, 2) + 1* Math.pow(2, 3);  
+
+document.write("<br>El resultado es: "+numero +"<br><br>");
+
+document.write("<br>Numero 1010");
+
+numero = 0* Math.pow(2, 0) + 1* Math.pow(2, 1) + 0* Math.pow(2, 2) + 1* Math.pow(2, 3);  
+
+document.write("<br>El resultado es: "+numero +"<br><br>");
+
+document.write("<br>Numero 1011");
+
+numero = 1* Math.pow(2, 0) + 1* Math.pow(2, 1) + 0* Math.pow(2, 2) + 1* Math.pow(2, 3);  
+
+document.write("<br>El resultado es: "+numero +"<br><br>");
+
+document.write("<br>Numero 1100");
+
+numero = 0* Math.pow(2, 0) + 0* Math.pow(2, 1) + 1* Math.pow(2, 2) + 1* Math.pow(2, 3);  
+
+document.write("<br>El resultado es: "+numero +"<br><br>");
+
+document.write("<br>Numero 1101");
+
+numero = 1* Math.pow(2, 0) + 0* Math.pow(2, 1) + 1* Math.pow(2, 2) + 1* Math.pow(2, 3);  
+
+document.write("<br>El resultado es: "+numero +"<br><br>");
+document.write("<br>Numero 1110");
+
+numero = 0* Math.pow(2, 0) + 1* Math.pow(2, 1) + 1* Math.pow(2, 2) + 1* Math.pow(2, 3);  
+
+document.write("<br>El resultado es: "+numero +"<br><br>");
+
+document.write("<br>Numero 1111");
+
+numero = 1* Math.pow(2, 0) + 1* Math.pow(2, 1) + 1* Math.pow(2, 2) + 1* Math.pow(2, 3);  
+
+document.write("<br>El resultado es: "+numero +"<br><br>");
